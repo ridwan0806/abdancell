7b81ebf31f8aa03c0a327609e2343d26f00a6c93
ridwan0806 <ridwanm32@gmail.com>
2022-02-26 20:23:42 +0700

    menampilkan view produk

--- lihat_produk.php
+++ lihat_produk.php
@@ -1,3 +1,9 @@
+<?php
+include ('resource.php');
+$produk = new Abdancell();
+$show = $produk->lihat_produk();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +12,34 @@
 	<title>halaman produk</title>
 </head>
 <body>
-
+	<table border="1">
+		<tr>
+			<td>NO</td>
+			<td>ID</td>
+			<td>Produk</td>
+			<td>Kategori</td>
+			<td>Branch</td>
+			<td>HPP</td>
+			<td>Harga Jual</td>
+			<td>Stok</td>
+		</tr>
+		<tr>
+			<?php
+				$no = 1;
+				foreach($show as $row){
+			?>
+			<td><?php echo $no++; ?></td>
+			<td><?php echo $row['kode_barang']; ?></td>
+			<td><?php echo $row['nama_barang']; ?></td>
+			<td><?php echo $row['kategori']; ?></td>
+			<td><?php echo $row['brand']; ?></td>
+			<td><?php echo $row['hpp']; ?></td>
+			<td><?php echo $row['harga_jual']; ?></td>
+			<td><?php echo $row['stok']; ?></td>
+		</tr>
+			<?php
+		}
+			?>
+	</table>
 </body>
 </html>
--- resource.php
+++ resource.php
@@ -8,6 +8,18 @@
 
 		public function __construct(){
 			$this->conn = mysqli_connect($this->host,$this->user,$this->pass,$this->db);
+			if(mysqli_connect_errno()){
+				echo "koneksi gagal" . mysqli_connect_errno;
+			}
+		}
+
+		public function lihat_produk(){
+			$sql = mysqli_query($this->conn,"select * from barang");
+			while($row = mysqli_fetch_array($sql))
+			{
+				$result[] = $row;
+			}
+			return $result;
 		}
 	}
 ?>
